9b76aaf3ee9ffd57f6c6fa86c909ee1f594e6c75
artanton <kabletrade@gmail.com>
2024-08-23 12:47:00 +0300

    nest-mongoose

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    await app.listen(3000);
+
+    console.log(`Database connection successful. Use port: 3000`);
+  } catch (err) {
+    console.log(err.message);
+    process.exit(1);
+  }
 }
 bootstrap();
